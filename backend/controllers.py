f754cad7de1121cde0813991c32ad037ac03918f
n-kaushik1 <nikanshkashik@gmail.com>
2024-07-18 12:27:53 +0530

    commit 18-07-24

--- backend/controllers.py
+++ backend/controllers.py
@@ -71,6 +71,9 @@ def Influencerrregistration():
      
     return render_template("Influencerregistration.html")
 
+# @app.route("/adminsearch",methods=["GET","POST"])
+# def adminsearch():
+#     return render_template("adminsearch.html")
 
 
 
